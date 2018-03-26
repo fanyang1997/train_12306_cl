9ce8cf98ac2a85a57dc4251b8bd912bf70aeedf5
kaiser yang <joey.tai@protonmail.com>
2018-03-26 21:27:16 +0800

    add some file

--- tickets.py
+++ tickets.py
@@ -1,63 +1,56 @@
-#coding<; utf-8
+# coding:utf-8
 
-"""命令行火车票查看工具
-
+"""命令行火车票查看器
 Usage:
     tickets [-gdtkz] <from> <to> <date>
-
-Options:
-    -h, --help  显示帮助菜单
-    -g          高铁    
-    -d          动车
-    -t          特快
-    -k          快递
-    -z          直达
-
-Example:
-    tickets 北京 上海 2018-10-10
-    tickets -dg 成都 南京 2018-10-10
 """
-import json
+
 from docopt import docopt
 from stations import stations
 import requests
 from prettytable import PrettyTable
-from colorama import init, Fore 
+from colorama import init, Fore
+
+init()
 
 
-class TrainCollection:
+class TrainsCollection:
     header = '车次 车站 时间 历时 一等 二等 高级软卧 软卧 硬卧 硬座 无座'.split()
 
-    def __init__(self, available_trains, available_place, options):
+    def __init__(self, available_trains,available_place, options):
+        """查询的火车班次集合
+        :param available_trains: 一个列表, 包含可获得的火车班次, 每个
+                                 火车班次是一个字典
+        :param options: 查询的选项, 如高铁, 动车, etc...
+        """
         self.available_trains = available_trains
         self.available_place = available_place
         self.options = options
 
+    @property
     def trains(self):
         for raw_train in self.available_trains:
             raw_train_list = raw_train.split('|')
             train_no = raw_train_list[3]
             initial = train_no[0].lower()
             duration = raw_train_list[10]
-
             if not self.options or initial in self.options:
                 train = [
                     train_no,# train number
-                    '\n'.join([self.available_place[raw_train_list[6]],#始发站
-                               self.available_place[raw_train_list[7]]]),#终点站
-                    '\n'.join([raw_train_list[8],# 发车时间
-                               raw_train_list[9]]),# 到站时间
-                    duration,#时长
-                    raw_train_list[-6] if raw_train_list[-6] else '--',# 一等 
-                    raw_train_list[-7] if raw_train_list[-7] else '--',# 二等 
-                    raw_train_list[-15] if raw_train_list[-15] else '--',# 高级软卧
-                    raw_train_list[-8] if raw_train_list[-8] else '--',#  软卧
-                    raw_train_list[-14] if raw_train_list[-14] else '--',#硬卧
-                    raw_train_list[-11] if raw_train_list[-11] else '--',#硬座
-                    raw_train_list[-9] if raw_train_list[-9] else '--',#无座
+                    '\n'.join([Fore.LIGHTGREEN_EX + self.available_place[raw_train_list[6]] + Fore.RESET,
+                               Fore.LIGHTRED_EX + self.available_place[raw_train_list[7]] + Fore.RESET]),
+                    '\n'.join([Fore.LIGHTGREEN_EX + raw_train_list[8] + Fore.RESET,
+                               Fore.LIGHTRED_EX + raw_train_list[9] + Fore.RESET]),
+                    duration,
+                    raw_train_list[-6] if raw_train_list[-6] else '--',
+                    raw_train_list[-7] if raw_train_list[-7] else '--',
+                    raw_train_list[-15] if raw_train_list[-15] else '--',
+                    raw_train_list[-8] if raw_train_list[-8] else '--',
+                    raw_train_list[-14] if raw_train_list[-14] else '--',
+                    raw_train_list[-11] if raw_train_list[-11] else '--',
+                    raw_train_list[-9] if raw_train_list[-9] else '--',
                 ]
                 yield train
-
 
     def pretty_print(self):
         pt = PrettyTable()
@@ -65,6 +58,7 @@ class TrainCollection:
         for train in self.trains:
             pt.add_row(train)
         print(pt)
+
 
 def cli():
     """command-line interface"""
@@ -87,5 +81,5 @@ def cli():
     TrainsCollection(available_trains,available_place, options).pretty_print()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     cli()
